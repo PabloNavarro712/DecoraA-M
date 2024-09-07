541ce88ba1a881fa0638e89f33eb9f156c32cba5
Ariadna <ariadnakantun8@gmail.com>
2024-09-07 00:58:24 -0600

    Paola - Servicios

--- fronted/src/app/app.module.ts
+++ fronted/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ServicesComponent } from './modules/services/services.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
