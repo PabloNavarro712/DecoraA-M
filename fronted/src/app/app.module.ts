4b72138bbc8854bca11de1a302f62e1d6810f67b
Ricardo Antonio Soto Beh <99599968+RICARDOSOTO6@users.noreply.github.com>
2024-09-08 12:36:31 -0600

    Update app.module.ts

--- fronted/src/app/app.module.ts
+++ fronted/src/app/app.module.ts
@@ -4,24 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-<<<<<<< HEAD
 import { CarouselComponent } from './carousel/carousel.component';
 import { GaleriaComponent } from './galeria/galeria.component';
-=======
 import { ServicesComponent } from './modules/services/services.component';
->>>>>>> origin/main
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-<<<<<<< HEAD
     CarouselComponent,
-    GaleriaComponent
+    GaleriaComponent,
   
-=======
     ServicesComponent
->>>>>>> origin/main
   ],
   imports: [
     BrowserModule,
