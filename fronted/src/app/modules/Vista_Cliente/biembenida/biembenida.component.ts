54ff3fb4e5f229a5171d88367931ee6f33ec5628
RICARDOSOTO6 <Ricardosoto6@cecyte.edu.mx>
2024-09-13 11:17:26 -0600

    Personalización de la ventana emergente y botón flotante

---
+++ fronted/src/app/modules/Vista_Cliente/biembenida/biembenida.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biembenida',
+  templateUrl: './biembenida.component.html',
+  styleUrls: ['./biembenida.component.css']
+})
+export class BiembenidaComponent {
+
+}
