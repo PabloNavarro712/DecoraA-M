69262fea86cf72ba10e12d370004f1662a051af0
RICARDOSOTO6 <Ricardosoto6@cecyte.edu.mx>
2024-10-06 22:49:34 -0600

    actualizacion de galeria (otra vez)

--- fronted/src/app/modules/Vista_Cliente/login/login.component.ts
+++ fronted/src/app/modules/Vista_Cliente/login/login.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
- 
-  
+  isLoginVisible: boolean = true; // Por defecto, mostramos el formulario de inicio de sesión
 
+  toggleForm(event: Event) {
+    event.preventDefault(); // Evitar la acción predeterminada del enlace
+    event.stopPropagation(); // Evitar que el evento se propague al modal
+    this.isLoginVisible = !this.isLoginVisible; // Cambiar entre login y registro
+  }
 }
