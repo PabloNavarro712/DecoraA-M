1a0a836e8b092df9b1860e4200e5709729d89010
PabloNavarro712 <80002646+PabloNavarro712@users.noreply.github.com>
2024-10-28 13:34:34 -0600

    Se agregó la vista de faq al inicio de la visa clientes

--- fronted/src/app/app-routing.module.ts
+++ fronted/src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { PanelEdicionComponent } from './vista-admin/salpicadero/panel-edicion/p
 import { AdminTemplateComponent } from './admin-template/admin-template.component';
 import { ClientTemplateComponent } from './template_cliente/client-template/client-template.component';
 import { EditarServicioComponent } from './vista-admin/editar-servicio/editar-servicio.component';
+import { FaqComponent } from './vista-cliente/faq/faq.component';
 const routes: Routes = [
   // Rutas del cliente
   {
@@ -20,7 +21,8 @@ const routes: Routes = [
     children: [
       { path: 'inicio', component: InicioComponent },  // Vista de inicio
       { path: 'nuestra-historia', component: NuestraHistoriaComponent }, // Vista de nuestra historia
-      { path: 'servicios', component: ServiciosComponent } // Vista de servicios
+      { path: 'servicios', component: ServiciosComponent }, // Vista de servicios
+      { path: 'faq', component: FaqComponent } //Vista de preguntas frecuentes
     ]
   },
   // Rutas del administrador
